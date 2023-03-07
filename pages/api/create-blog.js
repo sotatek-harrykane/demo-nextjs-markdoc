bbae63ab3675bccac89428100d8fefa0cd1361fa
hquan2808 <hquan2808@gmail.com>
2023-03-07 10:55:57 +0700

    Update create-blog.js

--- pages/api/create-blog.js
+++ pages/api/create-blog.js
@@ -2,12 +2,18 @@ import fs from "fs";
 import matter from "gray-matter";
 
 export default async function handler(req, res) {
-    if (req.method === "POST" && req.url === '/api/create-blog') {
+    if (req.method === "POST" && req.url === "/api/create-blog") {
         const { data: frontmatter } = matter(req.body);
-        const title = frontmatter.title.replace(/[^\w\s]/gi, "").replace(/\s+/g, "-").toLowerCase().trim();
+        const title = frontmatter.title
+            .replace(/[^\w\s]/gi, "")
+            .replace(/\s+/g, "-")
+            .toLowerCase()
+            .trim();
         const category = frontmatter.category.toLowerCase();
         try {
-            fs.writeFileSync(`posts/${category}/${title}.md`, req.body);
+            fs.accessSync(`posts/${category}/${title}.md`, fs.constants.W_OK);
+            console.log("can write %s", path);
+            // fs.writeFileSync(`posts/${category}/${title}.md`, req.body);
             return res.status(200).json({ message: "Create Successfully!" });
         } catch (err) {
             console.error(err);
